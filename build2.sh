bc7ad0ba58ddbec5858b8460df42c8e2eb13a285
Alan Newton <rufeooo@gmail.com>
2025-06-03 06:27:30 -0700

    build2.sh: src/narsil.c src/bulk.h +autoconf

---
+++ build2.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+mkdir -p bin/
+clang src/narsil.c -O0 -g1 -I. -fno-omit-frame-pointer -ferror-limit=3 -Wno-implicit-function-declaration -Wno-implicit-int -Wno-return-type -lcurses -DTEST_OVERRIDE_PATHS -DHAVE_CONFIG_H -D_DEFAULT_SOURCE -D_XOPEN_SOURCE=600 -Isrc -o bin/narsil
