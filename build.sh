36ea144daf92009de6c2d426836e7afac52613a3
Alan Newton <rufeooo@gmail.com>
2025-06-08 10:28:14 -0700

    legacy/ for prior sources

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 mkdir -p bin /
-clang narsil.c -O0 -g1 -I. -fno-omit-frame-pointer -ferror-limit=3 -Wno-implicit-function-declaration -Wno-implicit-int -Wno-return-type -lncursesw -ltinfo -DTEST_OVERRIDE_PATHS -DHAVE_CONFIG_H -D_DEFAULT_SOURCE -D_XOPEN_SOURCE=600 -Isrc -o bin/narsil
+clang narsil.c -O0 -g1 -I. -fno-omit-frame-pointer -ferror-limit=3 -Wno-implicit-function-declaration -Wno-implicit-int -Wno-return-type -lncursesw -ltinfo -DTEST_OVERRIDE_PATHS -DHAVE_CONFIG_H -D_DEFAULT_SOURCE -D_XOPEN_SOURCE=600 -Ilegacy -o bin/narsil
